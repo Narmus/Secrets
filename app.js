8a90bf6fc65871315d5fb035e70df967f46035eb
Arun M S <narmusgaming@gmail.com>
2022-09-16 12:13:14 +0530

    added gitignore and dotenv to hide sensitive data. Changed secret code to keep the data hidden.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -12,10 +13,13 @@ app.use(
 );
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+const secret = "Thisisthesecretcode";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -30,7 +34,11 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register");
 });
+app.get("/logout", (req, res) => {
+  res.render("home");
+});
 
+// Register Page
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
@@ -57,7 +65,11 @@ app.post("/login", (req, res) => {
       if (foundUser) {
         if (foundUser.password === password) {
           res.render("secrets");
+        } else {
+          res.render("login");
         }
+      } else {
+        res.render("login");
       }
     }
   });
